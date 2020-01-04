0e3318aba433cc4123089ebe65c521b34354db43
Bryan Swagerty <bryantee@gmail.com>
2020-01-04 01:26:52 -0700

    Add packages to brew step

--- brew.sh
+++ brew.sh
@@ -13,16 +13,27 @@ brew upgrade
 # GNU utils
 brew install coreutils
 
+# The basics
+brew install git
+brew install htop
+brew install vim
+brew install watch
+
+# zsh
+brew install zsh zsh-completions
+
 # Main packages
 brew cask install alfred
 brew cask install google-chrome
+brew cask install firefox
 brew cask install spotify
 brew cask install slack
 brew cask install visual-studio-code
+brew cask install webstorm
 brew cask install iterm2
-brew install zsh zsh-completions
 brew install diff-so-fancy
 brew install node
+brew cask install lastpass
 
 # fonts
 brew tap caskroom/fonts
